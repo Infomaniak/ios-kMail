0bb331c0fe7abdd4ca12c775d6fd8cf2a2d4f57c
Ambroise Decouttere <decouttereambroise@gmail.com>
2022-02-08 14:12:04 +0100

    Initial commit

---
+++ mail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mail
+//
+//  Created by Ambroise Decouttere on 08/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
