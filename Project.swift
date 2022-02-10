f3ff156e08ea0d45f7f33e2e3752c59a3410a0fc
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2022-02-10 13:01:37 +0100

    Tuist added

--- Project.swift
+++ Project.swift
@@ -1,24 +1,24 @@
 import ProjectDescription
 
-let project = Project(name: "mail",
+let project = Project(name: "Mail",
                       packages: [
                           .package(url: "https://github.com/Infomaniak/ios-login.git", .upToNextMajor(from: "1.4.0")),
                           .package(url: "https://github.com/ProxymanApp/atlantis", .upToNextMajor(from: "1.3.0"))
                       ],
                       targets: [
-                          Target(name: "mail",
+                          Target(name: "Mail",
                                  platform: .iOS,
                                  product: .app,
                                  bundleId: "com.infomaniak.mail",
                                  deploymentTarget: .iOS(targetVersion: "15.0", devices: [.iphone, .ipad]),
-                                 infoPlist: "mail/Info.plist",
-                                 sources: "mail/**",
+                                 infoPlist: "Mail/Info.plist",
+                                 sources: "Mail/**",
                                  resources: [
-                                     "mail/**/*.storyboard",
-                                     "mail/**/*.xcassets",
-                                     "mail/**/*.strings",
-                                     "mail/**/*.stringsdict",
-                                     "mail/**/*.xib",
+                                     "Mail/**/*.storyboard",
+                                     "Mail/**/*.xcassets",
+                                     "Mail/**/*.strings",
+                                     "Mail/**/*.stringsdict",
+                                     "Mail/**/*.xib"
 //                                     "mail/**/*.json",
 //                                     "mail/**/*.css"
                                  ],
@@ -26,14 +26,13 @@ let project = Project(name: "mail",
                                      .package(product: "Atlantis"),
                                      .package(product: "InfomaniakLogin")
                                  ]),
-                          Target(name: "mailTests",
+                          Target(name: "MailTests",
                                  platform: .iOS,
                                  product: .unitTests,
                                  bundleId: "com.infomaniak.mail.tests",
-                                 infoPlist: "mailTests/Info.plist",
-                                 sources: "mailTests/**",
+                                 infoPlist: "MailTests/Info.plist",
+                                 sources: "MailTests/**",
                                  dependencies: [
-                                     .target(name: "mail")
+                                     .target(name: "Mail")
                                  ])
                       ])
-
