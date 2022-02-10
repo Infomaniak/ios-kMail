a9daaad2ae7afe18034382d55a43db090c332268
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2022-02-10 13:28:34 +0100

    reword Mail

--- mailTests/mailTests.swift
+++ mailTests/mailTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import mail
+@testable import Mail
 
-class mailTests: XCTestCase {
+class MailTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
