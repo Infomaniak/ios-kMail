04092e60ace18e58b9ccfdd70e9eaa75f94fbb1c
Valentin Perignon <valentin.perignon@infomaniak.com>
2022-04-04 10:14:43 +0200

    Init tests

---
+++ MailTests/MailApiTests.swift
@@ -0,0 +1,62 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import InfomaniakCore
+import InfomaniakLogin
+import XCTest
+
+@testable import MailCore
+
+class FakeTokenDelegate: RefreshTokenDelegate {
+    func didUpdateToken(newToken: ApiToken, oldToken: ApiToken) {}
+
+    func didFailRefreshToken(_ token: ApiToken) {}
+}
+
+final class MailApiTests: XCTestCase {
+    let currentApiFetcher: MailApiFetcher = {
+        let token = ApiToken(accessToken: Env.token,
+                             expiresIn: Int.max,
+                             refreshToken: "",
+                             scope: "",
+                             tokenType: "",
+                             userId: Env.userId,
+                             expirationDate: Date(timeIntervalSinceNow: TimeInterval(Int.max)))
+        return MailApiFetcher(token: token, delegate: FakeTokenDelegate())
+    }()
+
+    var mailboxes = [Mailbox]()
+
+    // MARK: - Tests setup
+
+    func setUpTest() async throws -> [Mailbox] {
+        return try await currentApiFetcher.mailboxes()
+    }
+
+    // MARK: - Tests methods
+
+    func testMailboxes() async throws {
+        let mailboxes = try await setUpTest()
+        XCTAssertTrue(!mailboxes.isEmpty)
+    }
+
+    func testFolders() async throws {
+        let mailboxes = try await setUpTest()
+    }
+}
