05ab42af26c330598bef30c1a37ea3e5e0b5f2b0
Elena Willen <elena.willen@infomaniak.com>
2022-04-07 16:40:58 +0200

    rearrange views

--- Mail/Views/SplitView.swift
+++ Mail/Views/SplitView.swift
@@ -67,36 +67,3 @@ struct SplitView: View {
         }
     }
 }
-
-struct ThreadList: UIViewControllerRepresentable {
-    var mailboxManager: MailboxManager
-    var folder: Folder
-    var isCompact: Bool
-
-    func makeUIViewController(context: Context) -> ThreadListViewController {
-        return ThreadListViewController(mailboxManager: mailboxManager, folder: folder, isCompact: isCompact)
-    }
-
-    func updateUIViewController(_ uiViewController: ThreadListViewController, context: Context) {
-        // Intentionally unimplemented...
-    }
-}
-
-struct DeviceRotationViewModifier: ViewModifier {
-    let action: (UIDeviceOrientation) -> Void
-
-    func body(content: Content) -> some View {
-        content
-            .onAppear()
-            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                action(UIDevice.current.orientation)
-            }
-    }
-}
-
-// A View wrapper to make the modifier easier to use
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        modifier(DeviceRotationViewModifier(action: action))
-    }
-}
