d7353591dc8b13e0282f007019b66c1731345425
Elena Willen <elena.willen@infomaniak.com>
2022-05-12 16:28:44 +0200

    fix bug sonar cloud

--- Mail/Views/Thread List/ThreadListViewModel.swift
+++ Mail/Views/Thread List/ThreadListViewModel.swift
@@ -56,16 +56,9 @@ typealias Thread = MailCore.Thread
             guard let folder = folder else { return }
             try await mailboxManager.threads(folder: folder.freeze(), filter: filter)
 
-            if folder.role == .draft {
-                if let cachedFolder = mailboxManager.getRealm().object(ofType: Folder.self, forPrimaryKey: folder.id) {
-                    threads = AnyRealmCollection(cachedFolder.threads.sorted(by: \.date, ascending: false))
-                    observeChanges()
-                }
-            } else {
-                if let cachedFolder = mailboxManager.getRealm().object(ofType: Folder.self, forPrimaryKey: folder.id) {
-                    threads = AnyRealmCollection(cachedFolder.threads.sorted(by: \.date, ascending: false))
-                    observeChanges()
-                }
+            if let cachedFolder = mailboxManager.getRealm().object(ofType: Folder.self, forPrimaryKey: folder.id) {
+                threads = AnyRealmCollection(cachedFolder.threads.sorted(by: \.date, ascending: false))
+                observeChanges()
             }
         } catch {
             print("Error while getting threads: \(error)")
