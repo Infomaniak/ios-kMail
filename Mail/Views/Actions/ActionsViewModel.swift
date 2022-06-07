b65d00a6907e19e9186dce745b09c473130008b6
Florentin Bekier <florentin.bekier@infomaniak.com>
2022-06-07 10:59:12 +0200

    Add translations

--- Mail/Views/Actions/ActionsViewModel.swift
+++ Mail/Views/Actions/ActionsViewModel.swift
@@ -25,22 +25,22 @@ struct Action: Identifiable {
     let title: String
     let icon: MailResourcesImages
 
-    static let delete = Action(id: 1, title: "Supprimer", icon: MailResourcesAsset.bin)
-    static let reply = Action(id: 2, title: "Répondre", icon: MailResourcesAsset.emailActionReply)
-    static let archive = Action(id: 3, title: "Archiver", icon: MailResourcesAsset.archives)
-    static let forward = Action(id: 4, title: "Transférer", icon: MailResourcesAsset.emailActionTransfer)
-    static let markAsRead = Action(id: 5, title: "Marquer comme lu", icon: MailResourcesAsset.envelope)
-    static let move = Action(id: 6, title: "Déplacer", icon: MailResourcesAsset.emailActionSend21)
-    static let postpone = Action(id: 7, title: "Reporter", icon: MailResourcesAsset.waitingMessage)
-    static let spam = Action(id: 8, title: "Spam", icon: MailResourcesAsset.spam)
-    static let block = Action(id: 9, title: "Bloquer l’expéditeur", icon: MailResourcesAsset.blockUser)
-    static let phishing = Action(id: 10, title: "Hammeçonnage", icon: MailResourcesAsset.fishing)
-    static let print = Action(id: 11, title: "Imprimer", icon: MailResourcesAsset.printText)
-    static let saveAsPDF = Action(id: 12, title: "Enregistrer en PDF", icon: MailResourcesAsset.fileDownload)
-    static let openIn = Action(id: 13, title: "Ouvrir dans", icon: MailResourcesAsset.sendTo)
-    static let createRule = Action(id: 14, title: "Créer une règle", icon: MailResourcesAsset.ruleRegle)
-    static let report = Action(id: 15, title: "Signaler un problème d’affichage", icon: MailResourcesAsset.feedbacks)
-    static let editMenu = Action(id: 16, title: "Modifier le menu", icon: MailResourcesAsset.editTools)
+    static let delete = Action(id: 1, title: MailResourcesStrings.buttonDelete, icon: MailResourcesAsset.bin)
+    static let reply = Action(id: 2, title: MailResourcesStrings.buttonReply, icon: MailResourcesAsset.emailActionReply)
+    static let archive = Action(id: 3, title: MailResourcesStrings.buttonArchive, icon: MailResourcesAsset.archives)
+    static let forward = Action(id: 4, title: MailResourcesStrings.buttonForward, icon: MailResourcesAsset.emailActionTransfer)
+    static let markAsRead = Action(id: 5, title: MailResourcesStrings.buttonMarkAsRead, icon: MailResourcesAsset.envelope)
+    static let move = Action(id: 6, title: MailResourcesStrings.buttonMove, icon: MailResourcesAsset.emailActionSend21)
+    static let postpone = Action(id: 7, title: MailResourcesStrings.buttonPostpone, icon: MailResourcesAsset.waitingMessage)
+    static let spam = Action(id: 8, title: MailResourcesStrings.buttonSpam, icon: MailResourcesAsset.spam)
+    static let block = Action(id: 9, title: MailResourcesStrings.buttonBlockSender, icon: MailResourcesAsset.blockUser)
+    static let phishing = Action(id: 10, title: MailResourcesStrings.buttonPhishing, icon: MailResourcesAsset.fishing)
+    static let print = Action(id: 11, title: MailResourcesStrings.buttonPrint, icon: MailResourcesAsset.printText)
+    static let saveAsPDF = Action(id: 12, title: MailResourcesStrings.buttonSavePDF, icon: MailResourcesAsset.fileDownload)
+    static let openIn = Action(id: 13, title: MailResourcesStrings.buttonOpenIn, icon: MailResourcesAsset.sendTo)
+    static let createRule = Action(id: 14, title: MailResourcesStrings.buttonCreateRule, icon: MailResourcesAsset.ruleRegle)
+    static let report = Action(id: 15, title: MailResourcesStrings.buttonReportDisplayProblem, icon: MailResourcesAsset.feedbacks)
+    static let editMenu = Action(id: 16, title: MailResourcesStrings.buttonEditMenu, icon: MailResourcesAsset.editTools)
 }
 
 enum ActionsTarget {
