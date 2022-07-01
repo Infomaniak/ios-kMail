1b60adc8dc4ef3683c10d2477f05c8c303672f49
Florentin Bekier <florentin.bekier@infomaniak.com>
2022-07-01 17:30:36 +0200

    Update account view UI

---
+++ Mail/Components/LargeButton.swift
@@ -0,0 +1,42 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import SwiftUI
+
+struct LargeButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .textStyle(.buttonPill)
+                .padding(.vertical, 10)
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
+        .buttonBorderShape(.roundedRectangle(radius: 16))
+        .padding(.horizontal, 24)
+    }
+}
+
+struct LargeButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LargeButton(title: "Button") { /* Preview */ }
+    }
+}
