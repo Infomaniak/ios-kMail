8f5e3c8b1341a1a8c2b079f0c0cd19b09ced2ee5
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2022-08-31 08:40:51 +0200

    Fix scrolling first appearance

--- Mail/Utils/FloatingPanelHelper.swift
+++ Mail/Utils/FloatingPanelHelper.swift
@@ -97,6 +97,8 @@ class DisplayedFloatingPanelState<State>: ObservableObject, FloatingPanelControl
     func createPanelContent<Content: View>(content: Content) {
         let content = content.introspectScrollView { [weak self] scrollView in
             self?.floatingPanel.trackAndObserve(scrollView: scrollView)
+            // This prevents erratic scrolling on first appearance
+            self?.floatingPanel.invalidateLayout()
         }
         floatingPanel.set(contentViewController: UIHostingController(rootView: content))
     }
