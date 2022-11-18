18bd71c3e6309c9c9776c79e1ee4d3292b7b5a66
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2022-11-18 18:08:31 +0100

    Call new routes

--- Mail/Views/Thread List/ThreadListViewModel.swift
+++ Mail/Views/Thread List/ThreadListViewModel.swift
@@ -158,16 +158,20 @@ class DateSection: Identifiable {
             switch folder.role {
             case .inbox, .sent, .draft:
                 if let inboxFolder = mailboxManager.getFolder(with: .inbox) {
-                    try await mailboxManager.messages(folder: inboxFolder.freezeIfNeeded())
+                    try await mailboxManager.threads(folder: inboxFolder.freezeIfNeeded())
+//                    try await mailboxManager.messages(folder: inboxFolder.freezeIfNeeded())
                 }
                 if let sentFolder = mailboxManager.getFolder(with: .sent) {
-                    try await mailboxManager.messages(folder: sentFolder.freezeIfNeeded())
+                    try await mailboxManager.threads(folder: sentFolder.freezeIfNeeded())
+//                    try await mailboxManager.messages(folder: sentFolder.freezeIfNeeded())
                 }
                 if let draftFolder = mailboxManager.getFolder(with: .draft) {
-                    try await mailboxManager.messages(folder: draftFolder.freezeIfNeeded())
+                    try await mailboxManager.threads(folder: draftFolder.freezeIfNeeded())
+//                    try await mailboxManager.messages(folder: draftFolder.freezeIfNeeded())
                 }
             default:
-                try await mailboxManager.messages(folder: folder.freezeIfNeeded())
+                try await mailboxManager.threads(folder: folder.freezeIfNeeded())
+//                try await mailboxManager.messages(folder: folder.freezeIfNeeded())
             }
         }
         isLoadingPage = false
