f6167decf1fbca6d8a8e38aa6bd575952cdcd58b
Valentin Perignon <valentin.perignon@icloud.com>
2022-12-22 10:36:45 +0100

    feat: Add theme colors

--- Mail/Views/Onboarding/IlluColors.swift
+++ Mail/Views/Onboarding/IlluColors.swift
@@ -17,12 +17,12 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import Lottie
 import Foundation
+import Lottie
 
 extension AnimationKeypath {
-    static func iphoneScreen(group: Int, fond: Int) -> Self {
-        return AnimationKeypath(keys: ["IPHONE SCREEN", "Groupe \(group)", "Fond \(fond)"])
+    static func iphoneScreen(group: Int, layer: Int = 1) -> Self {
+        return AnimationKeypath(keys: ["IPHONE SCREEN", "Groupe \(group)", "Fond \(layer)"])
     }
 }
 
@@ -31,29 +31,69 @@ struct IlluColors {
     let lightColor: String
     let darkColor: String
 
-    static let illuColors = [
-        IlluColors(keypath: .iphoneScreen(group: 18, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
-        IlluColors(keypath: .iphoneScreen(group: 22, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 25, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 26, fond: 1), lightColor: "#FAFAFA", darkColor: "#282828"),
-        IlluColors(keypath: .iphoneScreen(group: 27, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 28, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 29, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 30, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 31, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 32, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 33, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 34, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 35, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 36, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 37, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 38, fond: 1), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
-        IlluColors(keypath: .iphoneScreen(group: 39, fond: 1), lightColor: "#C6AC9F", darkColor: "#996452"),
-        IlluColors(keypath: .iphoneScreen(group: 44, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
-        IlluColors(keypath: .iphoneScreen(group: 49, fond: 1), lightColor: "#C6AC9F", darkColor: "#996452"),
-        IlluColors(keypath: .iphoneScreen(group: 50, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
-        IlluColors(keypath: .iphoneScreen(group: 62, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
-        IlluColors(keypath: .iphoneScreen(group: 68, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
-        IlluColors(keypath: .iphoneScreen(group: 70, fond: 1), lightColor: "#F5F5F5", darkColor: "#3E3E3E")
+    // MARK: - Default colors
+
+    static let allColors = [
+        IlluColors(keypath: .iphoneScreen(group: 18), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
+        IlluColors(keypath: .iphoneScreen(group: 22), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 25), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 26), lightColor: "#FAFAFA", darkColor: "#282828"),
+        IlluColors(keypath: .iphoneScreen(group: 27), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 28), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 29), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 30), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 31), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 32), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 33), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 34), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 35), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 36), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 37), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 38), lightColor: "#E0E0E0", darkColor: "#4C4C4C"),
+        IlluColors(keypath: .iphoneScreen(group: 39), lightColor: "#C6AC9F", darkColor: "#996452"),
+        IlluColors(keypath: .iphoneScreen(group: 44), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
+        IlluColors(keypath: .iphoneScreen(group: 49), lightColor: "#C6AC9F", darkColor: "#996452"),
+        IlluColors(keypath: .iphoneScreen(group: 50), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
+        IlluColors(keypath: .iphoneScreen(group: 62), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
+        IlluColors(keypath: .iphoneScreen(group: 68), lightColor: "#F5F5F5", darkColor: "#3E3E3E"),
+        IlluColors(keypath: .iphoneScreen(group: 70), lightColor: "#F5F5F5", darkColor: "#3E3E3E")
+    ]
+
+    // MARK: - Theme colors
+
+    static let pinkColors = [
+        IlluColors(keypath: .iphoneScreen(group: 1), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 2), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 3), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 4), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 5), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 6), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 9), lightColor: "#FF5B97", darkColor: "#EF0057"),
+        IlluColors(keypath: .iphoneScreen(group: 12), lightColor: "#AB2456", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 15), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 19), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 20), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 23), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 24), lightColor: "#BC0055", darkColor: "#D0759F"),
+        IlluColors(keypath: .iphoneScreen(group: 43), lightColor: "#BD95A7", darkColor: "#AE366D"),
+        IlluColors(keypath: .iphoneScreen(group: 48), lightColor: "#BF4C80", darkColor: "#E75F9C")
+    ]
+
+    static let blueColors = [
+        IlluColors(keypath: .iphoneScreen(group: 1), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 2), lightColor: "#0098FF", darkColor: "#0098FF"),
+        IlluColors(keypath: .iphoneScreen(group: 3), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 4), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 5), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 6), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 9), lightColor: "#69C9FF", darkColor: "#6DCBFF"),
+        IlluColors(keypath: .iphoneScreen(group: 12), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 15), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 19), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 20), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 23), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 24), lightColor: "#0098FF", darkColor: "#0177C7"),
+        IlluColors(keypath: .iphoneScreen(group: 43), lightColor: "#3981AA", darkColor: "#56AFE1"),
+        IlluColors(keypath: .iphoneScreen(group: 48), lightColor: "#289CDD", darkColor: "#0D7DBC")
     ]
 }
