3fa542a313be9d7a0ee0732deec2723631fcc2f8
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-01-06 13:19:26 +0100

    Update schema version

--- MailCore/Cache/MailboxManager.swift
+++ MailCore/Cache/MailboxManager.swift
@@ -71,7 +71,7 @@ public class MailboxManager: ObservableObject {
         let realmName = "\(mailbox.userId)-\(mailbox.mailboxId).realm"
         realmConfiguration = Realm.Configuration(
             fileURL: MailboxManager.constants.rootDocumentsURL.appendingPathComponent(realmName),
-            schemaVersion: 2,
+            schemaVersion: 3,
             deleteRealmIfMigrationNeeded: true,
             objectTypes: [
                 Folder.self,
