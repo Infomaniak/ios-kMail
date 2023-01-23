8ed1c889507c786865d5930ce10119f14f245fae
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-01-23 14:06:06 +0100

    fix: PR feedback

--- Mail/Views/Thread/MessageBodyView.swift
+++ Mail/Views/Thread/MessageBodyView.swift
@@ -29,14 +29,13 @@ struct MessageBodyView: View {
         VStack {
             if let body = message.body {
                 if body.type == "text/plain" {
-                    Text(message.body?.value ?? "")
+                    Text(body.value)
                         .textStyle(.body)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal, 16)
                 } else {
                     GeometryReader { proxy in
                         WebView(model: $model, dynamicHeight: $webViewHeight, proxy: proxy)
-                            .frame(height: webViewHeight)
                     }
                     .frame(height: webViewHeight)
                     .onAppear {
@@ -48,12 +47,7 @@ struct MessageBodyView: View {
                 }
             } else {
                 // Display a shimmer while the body is loading
-                Text(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum justo quis neque iaculis, eget vehicula metus vulputate. Duis sit amet tempor nisl. Nulla ac semper risus, nec rutrum elit. Maecenas sed volutpat urna. Vestibulum varius ac orci eu eleifend. Sed at ullamcorper odio. Donec sodales, nisl vel pellentesque scelerisque, ligula justo efficitur ex, non vestibulum nisi purus sit amet dui. Praesent ultricies orci et enim hendrerit posuere eget quis leo. Mauris sit amet sollicitudin mi. Suspendisse volutpat odio ante, quis elementum massa congue sed. Sed varius varius tempus."
-                )
-                .redacted(reason: .placeholder)
-                .shimmering()
-                .padding(.horizontal, 16)
+                ShimmerView()
             }
         }
     }
---
+++ Mail/Views/Thread/ShimmerView.swift
@@ -0,0 +1,37 @@
+//
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import SwiftUI
+
+struct ShimmerView: View {
+    var body: some View {
+        Text(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum justo quis neque iaculis, eget vehicula metus vulputate. Duis sit amet tempor nisl. Nulla ac semper risus, nec rutrum elit. Maecenas sed volutpat urna. Vestibulum varius ac orci eu eleifend. Sed at ullamcorper odio. Donec sodales, nisl vel pellentesque scelerisque, ligula justo efficitur ex, non vestibulum nisi purus sit amet dui. Praesent ultricies orci et enim hendrerit posuere eget quis leo. Mauris sit amet sollicitudin mi. Suspendisse volutpat odio ante, quis elementum massa congue sed. Sed varius varius tempus."
+        )
+        .redacted(reason: .placeholder)
+        .shimmering()
+        .padding(.horizontal, 16)
+    }
+}
+
+struct ShimmerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShimmerView()
+    }
+}
