cf402cb7fd8ed6c151bb4f11acb5f18429b5e365
Elena Willen <elena.willen@infomaniak.com>
2023-01-27 12:01:57 +0100

    refactor(AccountCellView): Remove background fill for light mode

--- Mail/Views/Switch User/AccountCellView.swift
+++ Mail/Views/Switch User/AccountCellView.swift
@@ -35,10 +35,8 @@ struct AccountCellView: View {
 
     var body: some View {
         ZStack {
-            if isSelected {
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(MailResourcesAsset.backgroundSecondaryColor.swiftUiColor)
-            }
+            RoundedRectangle(cornerRadius: 10)
+                .fill(MailResourcesAsset.backgroundToolbarEditorColor.swiftUiColor)
 
             RoundedRectangle(cornerRadius: 10)
                 .stroke(MailResourcesAsset.separatorColor.swiftUiColor, lineWidth: 1)
