b1fb73a2df77a797171f1b0eebaf5a58adc5cc80
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-02-03 12:46:29 +0100

    refactor: Reformat

--- Mail/AppDelegate.swift
+++ Mail/AppDelegate.swift
@@ -32,10 +32,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var accountManager: AccountManager!
     static var orientationLock = UIInterfaceOrientationMask.all
 
-    func application(
-        _ application: UIApplication,
-        willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
+    func application(_ application: UIApplication,
+                     willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         Logging.initLogging()
         setupDI()
         DDLogInfo("Application starting in foreground ? \(UIApplication.shared.applicationState != .background)")
@@ -54,21 +52,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(
-        _ application: UIApplication,
+    func application(_ application: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
