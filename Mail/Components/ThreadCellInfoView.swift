d1650404abce6a0dc497db6cc046bd015f7c7364
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-03-09 15:26:56 +0100

    perf(ThreadCell): Factor out subviews

---
+++ Mail/Components/ThreadCellInfoView.swift
@@ -0,0 +1,38 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailCore
+import SwiftUI
+
+struct ThreadCellInfoView: View {
+    let dataHolder: ThreadCellDataHolder
+    let density: ThreadDensity
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(dataHolder.subject)
+                .textStyle(.body)
+                .lineLimit(1)
+
+            if density != .compact {
+                Text(dataHolder.preview)
+                    .textStyle(.bodySmallSecondary)
+                    .lineLimit(1)
+            }
+        }
+    }
+}
---
+++ Mail/Components/UnreadIndicatorView.swift
@@ -0,0 +1,35 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailCore
+import SwiftUI
+
+struct UnreadIndicatorView: View {
+    let hidden: Bool
+    var body: some View {
+        Circle()
+            .frame(width: Constants.unreadIconSize, height: Constants.unreadIconSize)
+            .foregroundColor(hidden ? .clear : .accentColor)
+    }
+}
+
+struct UnreadIndicatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        UnreadIndicatorView(hidden: false)
+    }
+}
