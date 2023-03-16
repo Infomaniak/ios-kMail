80a6bdc6ae33307370c492ac92009ff000d2002e
Valentin Perignon <valentin.perignon@infomaniak.com>
2023-03-16 14:03:26 +0100

    refactor: Use UserDefaults

--- Mail/Utils/View+Extension.swift
+++ Mail/Utils/View+Extension.swift
@@ -65,8 +65,7 @@ extension View {
 
     func navigationBarThreadListStyle() -> some View {
         if #available(iOS 16.0, *) {
-            @AppStorage(UserDefaults.shared.key(.accentColor)) var accentColor = DefaultPreferences.accentColor
-            return toolbarBackground(accentColor.navBarBackground.swiftUiColor, for: .navigationBar)
+            return toolbarBackground(UserDefaults.shared.accentColor.navBarBackground.swiftUiColor, for: .navigationBar)
         } else {
             return modifier(NavigationBarStyleViewModifier(
                 standardAppearance: BarAppearanceConstants.threadListNavigationBarAppearance,
