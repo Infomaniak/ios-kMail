a46f5c293d7dd729c4253f7687904f3d38e5c395
Valentin Perignon <valentin.perignon@infomaniak.com>
2023-04-06 13:33:25 +0200

    fix: Update appVersion constant -> "version XX-betaY" instead of "version XX-Alpha1 (Y)"

--- MailCore/Utils/Constants.swift
+++ MailCore/Utils/Constants.swift
@@ -138,7 +138,7 @@ public enum Constants {
     public static func appVersion() -> String {
         let release = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String? ?? "x.x"
         let build = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String? ?? "x"
-        return "kMail iOS version \(release)-Alpha1 (\(build))"
+        return "kMail iOS version \(release)-beta\(build)"
     }
 
     public static let searchFolderId = "search_folder_id"
