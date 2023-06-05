8a3d3c47a5ecdf0319e30a2fcf1e15c7b8ba1f7e
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2023-06-05 10:22:03 +0200

    chore: bump schema version

--- MailCore/Cache/MailboxManager.swift
+++ MailCore/Cache/MailboxManager.swift
@@ -72,7 +72,7 @@ public class MailboxManager: ObservableObject {
         let realmName = "\(mailbox.userId)-\(mailbox.mailboxId).realm"
         realmConfiguration = Realm.Configuration(
             fileURL: MailboxManager.constants.rootDocumentsURL.appendingPathComponent(realmName),
-            schemaVersion: 8,
+            schemaVersion: 9,
             deleteRealmIfMigrationNeeded: true,
             objectTypes: [
                 Folder.self,
