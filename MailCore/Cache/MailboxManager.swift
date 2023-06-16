243b6ae2078f0c739fb4cbaec973306290fef39e
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-06-16 09:38:14 +0200

    chore: Bump realm

--- MailCore/Cache/MailboxManager.swift
+++ MailCore/Cache/MailboxManager.swift
@@ -72,7 +72,7 @@ public class MailboxManager: ObservableObject {
         let realmName = "\(mailbox.userId)-\(mailbox.mailboxId).realm"
         realmConfiguration = Realm.Configuration(
             fileURL: MailboxManager.constants.rootDocumentsURL.appendingPathComponent(realmName),
-            schemaVersion: 9,
+            schemaVersion: 10,
             deleteRealmIfMigrationNeeded: true,
             objectTypes: [
                 Folder.self,
