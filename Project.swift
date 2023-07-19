65caad47ca6fac1eee95578368b296a5081d5b9f
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2023-07-19 18:12:58 +0200

    chore: Bump Core / Core UI

--- Project.swift
+++ Project.swift
@@ -24,7 +24,7 @@ let project = Project(name: "Mail",
                       packages: [
                           .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "4.0.0")),
                           .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "1.1.6")),
-                          .package(url: "https://github.com/Infomaniak/ios-core", .revision("0d43de51efcc97b212d73823506aa317ebfb8fa3")),
+                          .package(url: "https://github.com/Infomaniak/ios-core", .revision("60e101e3981ac5ce10cdd72ed8e60ebfdc0010cf")),
                           .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "2.5.1")),
                           .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "2.1.0")),
                           .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "1.1.0")),
