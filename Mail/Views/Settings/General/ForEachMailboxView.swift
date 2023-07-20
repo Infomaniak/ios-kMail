8c1d159b47dcb387de8b8b5a4c45e3af488e58ae
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-07-20 11:25:30 +0200

    refactor: Stop using AccountManager when fetching mailboxes

---
+++ Mail/Views/Settings/General/ForEachMailboxView.swift
@@ -0,0 +1,43 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailCore
+import RealmSwift
+import SwiftUI
+
+struct ForEachMailboxView<Content: View>: View {
+    @ObservedResults private var mailboxes: Results<Mailbox>
+
+    let content: (Mailbox) -> Content
+
+    init(userId: Int, excludedMailboxIds: [Int] = [], @ViewBuilder content: @escaping (Mailbox) -> Content) {
+        self.content = content
+        _mailboxes = ObservedResults(
+            Mailbox.self,
+            configuration: MailboxInfosManager.instance.realmConfiguration,
+            where: { $0.userId == userId && !$0.mailboxId.in(excludedMailboxIds) },
+            sortDescriptor: SortDescriptor(keyPath: \Mailbox.mailboxId)
+        )
+    }
+
+    var body: some View {
+        ForEach(mailboxes) { mailbox in
+            content(mailbox)
+        }
+    }
+}
