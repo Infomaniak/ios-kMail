a92d4397b61e3ae348fa82dd44b5d4baabbba80f
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-07-21 07:26:27 +0200

    chore: Update core

--- Project.swift
+++ Project.swift
@@ -29,7 +29,7 @@ let project = Project(name: "Mail",
                       packages: [
                           .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "4.0.0")),
                           .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "1.1.6")),
-                          .package(url: "https://github.com/Infomaniak/ios-core", .revision("ef2811a288a3a4b94dd5d04c5f47ddd771e4176c")),
+                          .package(url: "https://github.com/Infomaniak/ios-core", .revision("60e101e3981ac5ce10cdd72ed8e60ebfdc0010cf")),
                           .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "2.3.0")),
                           .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "3.0.0")),
                           .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "1.1.0")),
