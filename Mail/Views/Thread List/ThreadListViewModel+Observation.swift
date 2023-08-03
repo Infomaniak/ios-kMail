1d64fc2a9df7f217fa38475d95b448985632a1b6
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2023-08-03 09:53:35 +0200

    fix(UnreadFilter): Simplify unread filter

--- Mail/Views/Thread List/ThreadListViewModel+Observation.swift
+++ Mail/Views/Thread List/ThreadListViewModel+Observation.swift
@@ -125,8 +125,7 @@ extension ThreadListViewModel {
 
         let containAnyOf = NSPredicate(format: Self.containAnyOfUIDs, allThreadsUIDs)
         let realm = mailboxManager.getRealm()
-        let allThreads = realm.objects(Thread.self).filter(containAnyOf)
-        let oldThreads = allThreads.freezeIfNeeded()
+        let allThreads = realm.objects(Thread.self).filter(containAnyOf).sorted(by: \.date, ascending: false)
 
         observeFilteredThreadsToken = allThreads.observe(on: observeQueue) { [weak self] changes in
             guard let self else {
@@ -136,13 +135,23 @@ extension ThreadListViewModel {
             switch changes {
             case .initial:
                 break
-            case .update(let all, let deletionIndexes, _, let modificationIndexes):
-                refreshInFilterMode(
-                    all: all,
-                    old: oldThreads,
-                    deletions: deletionIndexes,
-                    changes: modificationIndexes
-                )
+            case .update(let all, _, _, _):
+                let filteredThreads = Array(all.freezeIfNeeded())
+                guard let newSections = sortThreadsIntoSections(threads: filteredThreads)
+                else { return }
+
+                DispatchQueue.main.sync {
+                    self.nextThreadIfNeeded(from: filteredThreads)
+                    self.filteredThreads = filteredThreads
+                    if self.filter != .all,
+                       filteredThreads.count == 1,
+                       !self.filter.accepts(thread: filteredThreads[0]) {
+                        self.filter = .all
+                    }
+                    withAnimation {
+                        self.sections = newSections
+                    }
+                }
             case .error:
                 break
             }
@@ -151,62 +160,6 @@ extension ThreadListViewModel {
 
     func stopObserveFilteredThreads() {
         observeFilteredThreadsToken?.invalidate()
-    }
-
-    /// Update filtered threads on observation change.
-    private func refreshInFilterMode(all: Results<Thread>, old: Results<Thread>, deletions: [Int], changes: [Int]) {
-        for index in changes {
-            let updatedThread = all[index]
-            let uid = updatedThread.uid
-
-            let threadToUpdate: Thread? = sections.reduce(nil as Thread?) { partialResult, section in
-                partialResult ?? section.threads.first { $0.uid == uid }
-            }
-
-            let sectionToUpdate = sections.first { section in
-                section.threads.contains { $0.uid == uid }
-            }
-
-            guard let threadToUpdate,
-                  let sectionToUpdate else {
-                continue
-            }
-
-            let threadToUpdateIndex = sectionToUpdate.threads.firstIndex(of: threadToUpdate)
-            guard let threadToUpdateIndex else {
-                continue
-            }
-
-            sectionToUpdate.threads[threadToUpdateIndex] = updatedThread.freeze()
-
-            Task {
-                await MainActor.run {
-                    objectWillChange.send()
-                }
-            }
-        }
-        for index in deletions {
-            let deletedThread = old[index]
-            let uid = deletedThread.uid
-
-            let sectionToUpdate = sections.first { section in
-                section.threads.contains { $0.uid == uid }
-            }
-
-            guard let sectionToUpdate else {
-                continue
-            }
-
-            sectionToUpdate.threads.removeAll {
-                $0.uid == uid
-            }
-
-            Task {
-                await MainActor.run {
-                    objectWillChange.send()
-                }
-            }
-        }
     }
 
     // MARK: - Observe unread count
