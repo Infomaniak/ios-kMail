628bef9a2f08c40834da71cae39f323d09403e5f
Valentin Perignon <valentin.perignon@infomaniak.com>
2023-10-11 10:23:25 +0200

    feat(ApiFetcher): Increase timeout

--- MailCore/Utils/Constants.swift
+++ MailCore/Utils/Constants.swift
@@ -54,6 +54,8 @@ public enum Constants {
 
     public static let matomoId = "9"
 
+    public static let longTimeout: TimeInterval = 120
+
     public static let emailPredicate = NSPredicate(format: "SELF MATCHES %@", Constants.mailRegex)
     public static let mailRegex =
         "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
