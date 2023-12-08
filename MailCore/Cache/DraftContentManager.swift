9ca3002e694b33f4d48d32fc49625e79726fd87a
Valentin Perignon <valentin.perignon@infomaniak.com>
2023-12-08 11:25:17 +0100

    feat: Add forward

--- MailCore/Cache/DraftContentManager.swift
+++ MailCore/Cache/DraftContentManager.swift
@@ -242,7 +242,30 @@ extension DraftContentManager {
     private func formatForward(message: Message) throws -> String {
         guard let formattedMessage = try extractHTMLFromReplyingBody(of: message) else { return "" }
 
-        return formattedMessage
+        let date = DateFormatter.localizedString(from: message.date, dateStyle: .medium, timeStyle: .short)
+        let to = ListFormatter.localizedString(byJoining: message.to.map(\.htmlDescription))
+        let subject = message.formattedSubject.normalizedApostrophes
+        var cc: String {
+            if !message.cc.isEmpty {
+                return "<div>\(MailResourcesStrings.Localizable.ccTitle) \(ListFormatter.localizedString(byJoining: message.cc.map(\.htmlDescription)))<br></div>"
+            } else {
+                return ""
+            }
+        }
+
+        return """
+        <div class="\(Constants.forwardQuoteHTMLClass)">
+            <div>---------- \(MailResourcesStrings.Localizable.messageForwardHeader) ---------<br></div>
+            <div>\(MailResourcesStrings.Localizable.fromTitle) \(message.from.first?.htmlDescription ?? "")<br></div>
+            <div>\(MailResourcesStrings.Localizable.dateTitle) \(date)<br></div>
+            <div>\(MailResourcesStrings.Localizable.subjectTitle) \(subject)<br></div>
+            <div>\(MailResourcesStrings.Localizable.toTitle) \(to)<br></div>
+            \(cc)
+            <div><br></div>
+            <div><br></div>
+            \(formattedMessage)
+        </div>
+        """
     }
 
     private func extractHTMLFromReplyingBody(of message: Message) throws -> String? {
