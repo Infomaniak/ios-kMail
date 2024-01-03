b05c0e21143527d1fc4100be9fdad08edb8eb319
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-01-03 16:57:56 +0100

    feat: Update attendees list with cells

---
+++ Mail/Views/Bottom sheets/Calendar/CalendarAttendeeCell.swift
@@ -0,0 +1,51 @@
+//
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2022 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailCore
+import SwiftUI
+
+struct CalendarAttendeeCell: View {
+    let recipient: Recipient
+
+    var body: some View {
+        HStack(spacing: UIPadding.regular) {
+            CalendarAttendeeAvatarView(recipient: recipient, choice: .yes)
+
+            VStack(alignment: .leading, spacing: 0) {
+                if !recipient.name.isEmpty && recipient.name != recipient.email {
+                    Text(recipient.name)
+                        .textStyle(.bodyMedium)
+                }
+                Text(recipient.email)
+                    .textStyle(.bodySecondary)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(value: .regular)
+    }
+}
+
+#Preview {
+    List {
+        CalendarAttendeeCell(recipient: PreviewHelper.sampleRecipient1)
+        CalendarAttendeeCell(recipient: Recipient(email: "tim@ik.com", name: "tim@ik.com"))
+    }
+    .listStyle(.plain)
+    .environmentObject(PreviewHelper.sampleMailboxManager)
+}
