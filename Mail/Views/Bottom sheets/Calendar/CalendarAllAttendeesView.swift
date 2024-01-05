a09f46bd9ea457e64f89f0d48475ad5ae49a966e
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-01-05 09:51:29 +0100

    feat: Show attendees sheet correctly

--- Mail/Views/Bottom sheets/Calendar/CalendarAllAttendeesView.swift
+++ Mail/Views/Bottom sheets/Calendar/CalendarAllAttendeesView.swift
@@ -25,19 +25,20 @@ struct CalendarAllAttendeesView: View {
     let attendees: [Recipient]
 
     var body: some View {
-        NBNavigationStack {
-            List(attendees) { attendee in
+        VStack(spacing: 0) {
+            Text(MailResourcesStrings.Localizable.attendeesListTitle(attendees.count))
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom, value: .small)
+
+            ForEach(attendees) { attendee in
                 CalendarAttendeeCell(recipient: attendee)
-                    .threadListCellAppearance()
                     .overlay(alignment: .bottom) {
                         if attendee != attendees.last {
                             IKDivider()
                         }
                     }
             }
-            .listStyle(.plain)
-            .navigationTitle(MailResourcesStrings.Localizable.attendeesListTitle(attendees.count))
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
