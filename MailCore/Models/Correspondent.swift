bd42b1f0ef83da533a455560a494e4cbead05daf
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-01-15 15:51:11 +0100

    feat: Init CommonContact with Correspondent

--- MailCore/Models/Correspondent.swift
+++ MailCore/Models/Correspondent.swift
@@ -18,7 +18,7 @@
 
 import Foundation
 
-public protocol Correspondent: Identifiable {
+public protocol Correspondent: Identifiable where ID == String {
     var email: String { get }
     var name: String { get }
 
