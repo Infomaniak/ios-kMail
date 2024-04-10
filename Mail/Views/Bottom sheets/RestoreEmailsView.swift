f21c634ca7b36f9303066ffb324a1f9cf1760d7e
Charlène Hoareau <charlene.hoareau@infomaniak.com>
2024-04-10 11:21:58 +0200

    fix: Remove useless Vstack

--- Mail/Views/Bottom sheets/RestoreEmailsView.swift
+++ Mail/Views/Bottom sheets/RestoreEmailsView.swift
@@ -26,55 +26,49 @@ import SwiftUI
 struct RestoreEmailsView: View {
     @EnvironmentObject var mailboxManager: MailboxManager
 
-    @Environment(\.dismiss) private var dismiss
-
     @State private var selectedDate = ""
     @State private var availableDates = [String]()
     @State private var pickerNoSelectionText = MailResourcesStrings.Localizable.loadingText
 
     @LazyInjectService private var matomo: MatomoUtils
     @LazyInjectService private var snackbarPresenter: SnackBarPresentable
-    @LazyInjectService private var platformDetector: PlatformDetectable
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text(MailResourcesStrings.Localizable.restoreEmailsTitle)
                 .textStyle(.bodyMedium)
                 .padding(.bottom, 16)
-                .padding(.horizontal, UIPadding.bottomSheetHorizontal)
 
-            VStack(alignment: .leading, spacing: 0) {
-                Text(MailResourcesStrings.Localizable.restoreEmailsText)
-                    .textStyle(.bodySecondary)
-                    .padding(.bottom, 10)
+            Text(MailResourcesStrings.Localizable.restoreEmailsText)
+                .textStyle(.bodySecondary)
+                .padding(.bottom, 10)
 
-                LargePicker(title: MailResourcesStrings.Localizable.restoreEmailsBackupDate,
-                            noSelectionText: pickerNoSelectionText,
-                            selection: $selectedDate,
-                            items: availableDates.map(mapDates))
-                    .padding(.bottom, 24)
-                    .onChange(of: selectedDate) { _ in
-                        matomo.track(eventWithCategory: .restoreEmailsBottomSheet, action: .input, name: "selectDate")
-                    }
+            LargePicker(title: MailResourcesStrings.Localizable.restoreEmailsBackupDate,
+                        noSelectionText: pickerNoSelectionText,
+                        selection: $selectedDate,
+                        items: availableDates.map(mapDates))
+                .padding(.bottom, 24)
+                .onChange(of: selectedDate) { _ in
+                    matomo.track(eventWithCategory: .restoreEmailsBottomSheet, action: .input, name: "selectDate")
+                }
 
-                ModalButtonsView(primaryButtonTitle: MailResourcesStrings.Localizable.buttonConfirmRestoreEmails,
-                                 secondaryButtonTitle: nil,
-                                 primaryButtonEnabled: !availableDates.isEmpty,
-                                 primaryButtonAction: restoreEmails)
-            }
-            .padding(.horizontal, UIPadding.bottomSheetHorizontal)
-            .task {
-                await tryOrDisplayError {
-                    let backupsList = try await mailboxManager.apiFetcher.listBackups(mailbox: mailboxManager.mailbox).backups
-                    withAnimation {
-                        availableDates = backupsList
-                        selectedDate = backupsList.last ?? ""
-                        pickerNoSelectionText = MailResourcesStrings.Localizable.pickerNoSelection
+            ModalButtonsView(primaryButtonTitle: MailResourcesStrings.Localizable.buttonConfirmRestoreEmails,
+                             secondaryButtonTitle: nil,
+                             primaryButtonEnabled: !availableDates.isEmpty,
+                             primaryButtonAction: restoreEmails)
+                .task {
+                    await tryOrDisplayError {
+                        let backupsList = try await mailboxManager.apiFetcher.listBackups(mailbox: mailboxManager.mailbox).backups
+                        withAnimation {
+                            availableDates = backupsList
+                            selectedDate = backupsList.last ?? ""
+                            pickerNoSelectionText = MailResourcesStrings.Localizable.pickerNoSelection
+                        }
                     }
                 }
-            }
-            .matomoView(view: [MatomoUtils.View.bottomSheet.displayName, "RestoreEmailsView"])
+                .matomoView(view: [MatomoUtils.View.bottomSheet.displayName, "RestoreEmailsView"])
         }
+        .padding(.horizontal, UIPadding.bottomSheetHorizontal)
     }
 
     private func restoreEmails() async {
