f6371da00cd857957a7a2b2e3400dd7ca9963181
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2024-04-24 13:26:32 +0200

    fix: Swift format

--- MailCore/API/Endpoint.swift
+++ MailCore/API/Endpoint.swift
@@ -133,7 +133,7 @@ public extension Endpoint {
     static func signatures(hostingId: Int, mailboxName: String) -> Endpoint {
         return .baseManagerMailbox(hostingId: hostingId, mailboxName: mailboxName).appending(path: "/signatures")
     }
-    
+
     static func updateSignature(hostingId: Int, mailboxName: String) -> Endpoint {
         return .signatures(hostingId: hostingId, mailboxName: mailboxName).appending(path: "/set_defaults")
     }
