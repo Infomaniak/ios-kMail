35e753661b2c722400a764368178a5cb3e9b145c
Charlène Hoareau <charlene.hoareau@infomaniak.com>
2024-04-25 15:37:08 +0200

    feat: Add UpdateVersion view and alert

---
+++ Mail/Views/Thread List/UpdateVersionAlertView.swift
@@ -0,0 +1,65 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2024 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import InfomaniakCoreUI
+import InfomaniakDI
+import MailCore
+import MailResources
+import SwiftUI
+
+struct UpdateVersionAlertView: View {
+    @LazyInjectService private var matomo: MatomoUtils
+
+    @Binding var isShowingUpdateVersionView: Bool
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: UIPadding.medium) {
+            Text("Vos e-mails peuvent ne pas s’afficher correctement ")
+                .textStyle(.bodyMedium)
+            Text("Pour une expérience optimale, veuillez mettre à jour votre appareil")
+                .textStyle(.body)
+
+            ModalButtonsView(primaryButtonTitle: "Mettre à jour",
+                             // MailResourcesStrings.Localizable.buttonConfirmRestoreEmails,
+                             secondaryButtonTitle: "Plus tard",
+                             primaryButtonAction: updateVersion,
+                             secondaryButtonAction: dismissUpdateVersionView)
+        }
+    }
+
+    private func updateVersion() {
+        matomo.track(eventWithCategory: .syncAutoConfig, name: "openSettings")
+        @Environment(\.openURL) var openURL
+        //        @InjectService var platformDetector: PlatformDetectable
+        let url: URL
+        //        if platformDetector.isMac {
+        //            url = DeeplinkConstants.macSecurityAndPrivacy
+        //        } else {
+        url = DeeplinkConstants.iosPreferences
+//            URL(string: "x-apple.systempreferences:com.apple.preference.security")
+        //        }
+        openURL(url)
+    }
+
+    private func dismissUpdateVersionView() {
+        isShowingUpdateVersionView = false
+    }
+}
+
+#Preview {
+    UpdateVersionAlertView(isShowingUpdateVersionView: .constant(true))
+}
