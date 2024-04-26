9a95807148c5fbec3c32d94b7461792362147f78
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-04-26 14:30:38 +0200

    ci: Add periphery scan script

---
+++ scripts/periphery.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#tuist install
+#tuist generate -n
+
+
+# Check if the "--full-dir" argument is provided
+if [[ "$1" == "--full-dir" ]]; then
+    full_dir=true
+else
+    full_dir=false
+fi
+
+# Perform the periphery scan and handle directory removal based on argument
+if "$full_dir"; then
+    detailedOutput=$(periphery scan --quiet)
+else
+    detailedOutput=$(periphery scan --quiet | sed "s|$(pwd)/||g")
+fi
+
+unusedCount=$(wc -l <<< "$detailedOutput" | tr -d '[:space:]')
+
+echo "$detailedOutput"
+echo "Total unused instances $unusedCount"
