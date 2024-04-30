b95a3c34d00788e9a5eceeffeed2d51666b935f7
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-04-30 10:34:01 +0200

    fix: Avoid useless bool in script

--- scripts/periphery.sh
+++ scripts/periphery.sh
@@ -6,15 +6,8 @@ tuist install
 tuist generate -n
 
 
-# Check if the "--full-dir" argument is provided
-if [[ "$1" == "--full-dir" ]]; then
-    full_dir=true
-else
-    full_dir=false
-fi
-
 # Perform the periphery scan and handle directory removal based on argument
-if "$full_dir"; then
+if [[ "$1" == "--full-dir" ]]; then
     detailedOutput=$(periphery scan --quiet --enable-unused-import-analysis)
 else
     detailedOutput=$(periphery scan --quiet --enable-unused-import-analysis | sed "s|$(pwd)/||g")
