4829f6c1313ed2e2163dc47fc5ab8e5fbbcdca30
Elena Willen <elena.willen@infomaniak.com>
2024-05-08 13:40:35 +0200

    fix: Remove sync from settings for macOS

--- Mail/Views/Settings/SettingsView.swift
+++ Mail/Views/Settings/SettingsView.swift
@@ -89,17 +89,19 @@ struct SettingsView: View {
 
                     // MARK: Sync Calendar/Contacts
 
-                    Button {
-                        matomo.track(eventWithCategory: .syncAutoConfig, name: "openFromSettings")
-                        if platformDetector.isMac {
-                            isShowingSyncProfile = true
-                        } else {
-                            mainViewState.isShowingSyncProfile = true
+                    if !platformDetector.isMac {
+                        Button {
+                            matomo.track(eventWithCategory: .syncAutoConfig, name: "openFromSettings")
+                            if platformDetector.isMac {
+                                isShowingSyncProfile = true
+                            } else {
+                                mainViewState.isShowingSyncProfile = true
+                            }
+                        } label: {
+                            SettingsSubMenuLabel(title: MailResourcesStrings.Localizable.syncCalendarsAndContactsTitle)
                         }
-                    } label: {
-                        SettingsSubMenuLabel(title: MailResourcesStrings.Localizable.syncCalendarsAndContactsTitle)
+                        .buttonStyle(.plain)
                     }
-                    .buttonStyle(.plain)
 
                     // MARK: AI Writer
 
