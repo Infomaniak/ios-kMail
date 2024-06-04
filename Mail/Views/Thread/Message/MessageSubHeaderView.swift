aa38767f9b17492b007793c646b5a7619a99b08e
Elena Willen <elena.willen@infomaniak.com>
2024-06-04 16:35:24 +0200

    fix: After merge

--- Mail/Views/Thread/Message/MessageSubHeaderView.swift
+++ Mail/Views/Thread/Message/MessageSubHeaderView.swift
@@ -55,7 +55,7 @@ struct MessageSubHeaderView: View {
                 .padding(.horizontal, value: .regular)
         }
 
-        if !message.attachments.filter({ $0.disposition == .attachment || $0.contentId == nil }).isEmpty {
+        if !message.attachments.filter({ $0.disposition == .attachment || $0.contentId == nil }).isEmpty || message.swissTransferUuid != nil {
             AttachmentsView(message: message)
         }
     }
