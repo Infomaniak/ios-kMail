dbe644a63d98c650d8a6a1d1aa3827ee6b8d3578
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-09-20 09:11:20 +0200

    fix: Expose InfomaniakDI as Framework

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -8,7 +8,8 @@ import ProjectDescriptionHelpers
 let packageSettings = PackageSettings(
     productTypes: [
         "RealmSwift": .staticLibrary,
-        "Realm": .staticLibrary
+        "Realm": .staticLibrary,
+        "InfomaniakDI": .framework
     ]
 )
 
