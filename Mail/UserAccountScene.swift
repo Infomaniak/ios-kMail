0e03074f9a2f51fc49cd3dff2ed951e16db66db7
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2024-09-26 08:26:01 +0200

    fix: Don't increase app launch if onboarding OR preloading

--- Mail/UserAccountScene.swift
+++ Mail/UserAccountScene.swift
@@ -76,7 +76,7 @@ struct UserAccountScene: Scene {
     }
 
     private func willEnterForeground() {
-        if rootViewState.state != .onboarding {
+        if rootViewState.state != .onboarding && rootViewState.state != .preloading {
             appLaunchCounter.increase()
             reviewManager.decreaseOpeningUntilReview()
         }
