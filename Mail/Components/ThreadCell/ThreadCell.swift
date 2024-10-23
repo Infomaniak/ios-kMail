55729e430326ba67c565c3ca999b2555c8cd125e
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2024-10-23 17:29:52 +0200

    refactor: Same date view for thread + feat: Icon for scheduled messages

--- Mail/Components/ThreadCell/ThreadCell.swift
+++ Mail/Components/ThreadCell/ThreadCell.swift
@@ -44,7 +44,7 @@ struct ThreadCellDataHolder {
     let recipientToDisplay: Recipient?
 
     /// Date of the last message of the folder, otherwise the last message of the thread
-    let date: String
+    let date: Date
 
     /// Subject of the first message
     let subject: String
@@ -58,7 +58,7 @@ struct ThreadCellDataHolder {
         // swiftlint:disable:next last_where
         let lastMessageNotFromSent = thread.messages.filter(Self.lastMessageNotFromSentPredicate).last ?? thread.messages.last
 
-        date = thread.date.formatted(.thread(.list))
+        date = thread.date
 
         subject = thread.formattedSubject
 
@@ -181,7 +181,7 @@ struct ThreadCell: View {
                     ),
                     messageCount: thread.messages.count,
                     prominentMessageCount: thread.hasUnseenMessages,
-                    formattedDate: dataHolder.date,
+                    date: dataHolder.date,
                     showDraftPrefix: thread.hasDrafts
                 )
 
