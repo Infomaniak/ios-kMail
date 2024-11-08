233e160ac4316fcb67da082fdfb631e17bb89921
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2024-11-08 09:27:26 +0100

    fix: Modifying schedule date

--- Mail/Views/New Message/ComposeMessageView.swift
+++ Mail/Views/New Message/ComposeMessageView.swift
@@ -250,7 +250,11 @@ struct ComposeMessageView: View {
                 mainViewState.isShowingReviewAlert = reviewManager.shouldRequestReview()
             }
 
-            draftManager.syncDraft(mailboxManager: mailboxManager, showSnackbar: shouldShowSnackbar, changeFolderAction: mainViewState.changeSelectedFolder)
+            draftManager.syncDraft(
+                mailboxManager: mailboxManager,
+                showSnackbar: shouldShowSnackbar,
+                changeFolderAction: mainViewState.changeSelectedFolder
+            )
         }
         .customAlert(item: $isShowingAlert) { alert in
             switch alert.type {
