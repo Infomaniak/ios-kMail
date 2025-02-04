902e27afefd34f16927aaac32ec21811198106d6
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-02-04 12:59:41 +0100

    feat: Add endpoints

---
+++ MailCore/API/Endpoint/Endpoint+Snooze.swift
@@ -0,0 +1,29 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import InfomaniakCore
+
+public extension Endpoint {
+    static func snooze(uuid: String) -> Endpoint {
+        return .mailbox(uuid: uuid).appending(path: "snoozes")
+    }
+
+    static func snoozeAction(resource: String) -> Endpoint {
+        return .resource(resource)
+    }
+}
