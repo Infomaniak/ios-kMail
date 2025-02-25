32a1032309a47c8e4c660c436be109b85104faab
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-02-25 09:57:03 +0100

    feat: Show only the meeting room if the event is in the premises

--- MailCore/Cache/MailboxManager/MailboxManager.swift
+++ MailCore/Cache/MailboxManager/MailboxManager.swift
@@ -75,7 +75,7 @@ public final class MailboxManager: ObservableObject, MailboxManageable {
         let realmName = "\(mailbox.userId)-\(mailbox.mailboxId).realm"
         realmConfiguration = Realm.Configuration(
             fileURL: MailboxManager.constants.rootDocumentsURL.appendingPathComponent(realmName),
-            schemaVersion: 35,
+            schemaVersion: 36,
             migrationBlock: { migration, oldSchemaVersion in
                 // No migration needed from 0 to 16
                 if oldSchemaVersion < 17 {
@@ -113,7 +113,8 @@ public final class MailboxManager: ObservableObject, MailboxManageable {
                 Bimi.self,
                 SwissTransferAttachment.self,
                 File.self,
-                MessageUid.self
+                MessageUid.self,
+                BookableResource.self
             ]
         )
 
