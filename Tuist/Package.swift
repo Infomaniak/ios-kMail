56d0c5bfc97de5929dd7275d1a4228a8a77987ff
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-03-19 08:41:45 +0100

    chore: Bump DI to the version using an internal lock

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -52,7 +52,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.1.0")),
-        .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.2")),
+        .package(url: "https://github.com/Infomaniak/ios-dependency-injection.git", branch: "fix/race-condition-in-loadOrResolve"),
         .package(url: "https://github.com/Infomaniak/swift-concurrency", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-core", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "18.0.0")),
