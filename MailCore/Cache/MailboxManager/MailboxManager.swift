134c44aa3d710c638c81c5d8f94e3f65b945c4ff
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-04-09 08:56:00 +0200

    feat: Migration block

--- MailCore/Cache/MailboxManager/MailboxManager.swift
+++ MailCore/Cache/MailboxManager/MailboxManager.swift
@@ -119,10 +119,19 @@ public final class MailboxManager: ObservableObject, MailboxManageable {
                     }
                 }
                 if oldSchemaVersion < 40 {
-                    migration.enumerateObjects(ofType: Thread.className()) { oldObject, newObject in
-                        if let messages = newObject?["messages"] as? List<Message>,
-                           let lastMessageFromFolder = messages.last(where: { $0.folderId == newObject?["folderId"] as? String }) {
-                            newObject?["isLastMessageFromFolderSnoozed"] = lastMessageFromFolder.isSnoozed
+                    migration.enumerateObjects(ofType: Thread.className()) { _, newObject in
+                        if let messages = newObject?["messages"] as? List<MigrationObject>,
+                           let lastMessageFromFolder = messages.last(where: {
+                               $0["folderId"] as? String == newObject?["folderId"] as? String
+                            }) {
+                            let snoozeState = lastMessageFromFolder["snoozeState"] as? String?
+                            let snoozeEndDate = lastMessageFromFolder["snoozeEndDate"] as? Date?
+                            let snoozeUuid = lastMessageFromFolder["snoozeUuid"] as? String?
+                            let isSnoozed = snoozeState == SnoozeState.snoozed.rawValue && snoozeEndDate != nil && snoozeUuid != nil
+
+                            newObject?["isLastMessageFromFolderSnoozed"] = isSnoozed
+                        } else {
+                            print("coucou")
                         }
                     }
                 }
