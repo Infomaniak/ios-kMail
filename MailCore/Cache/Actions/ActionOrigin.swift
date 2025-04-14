266ab445f68ad2a0aef222f91c55f27bad7f406a
Jordan Chapelle <jordan.chapelle@infomaniak.com>
2025-04-14 09:49:41 +0200

    fix: Add contextMenu to floatingPanel origin

--- MailCore/Cache/Actions/ActionOrigin.swift
+++ MailCore/Cache/Actions/ActionOrigin.swift
@@ -34,6 +34,7 @@ public struct ActionOrigin {
         case threadList
         case messageList
         case messageDetails
+        case contextMenu
     }
 
     public private(set) var type: ActionOriginType
@@ -150,32 +151,5 @@ public struct ActionOrigin {
     public static func threadHeader(originFolder: Folder? = nil,
                                     nearestMessagesToSnooze: Binding<[Message]?>? = nil) -> ActionOrigin {
         return ActionOrigin(type: .threadHeader, folder: originFolder, nearestMessagesToSnooze: nearestMessagesToSnooze)
-    }
-
-    public static func contextMenu(originFolder: Folder? = nil,
-                                   nearestFlushAlert: Binding<FlushAlertState?>? = nil,
-                                   nearestMessagesToMoveSheet: Binding<[Message]?>? = nil,
-                                   nearestBlockSenderAlert: Binding<BlockRecipientAlertState?>? = nil,
-                                   nearestBlockSendersList: Binding<BlockRecipientState?>? = nil,
-                                   nearestReportJunkMessagesActionsPanel: Binding<[Message]?>? = nil,
-                                   nearestReportedForPhishingMessagesAlert: Binding<[Message]?>? = nil,
-                                   nearestReportedForDisplayProblemMessageAlert: Binding<Message?>? = nil,
-                                   nearestShareMailLinkPanel: Binding<ShareMailLinkResult?>? = nil,
-                                   nearestMessagesToSnooze: Binding<[Message]?>? = nil,
-                                   messagesToDownload: Binding<[Message]?>? = nil) -> ActionOrigin {
-        return ActionOrigin(
-            type: .contextMenu,
-            folder: originFolder,
-            nearestFlushAlert: nearestFlushAlert,
-            nearestMessagesToMoveSheet: nearestMessagesToMoveSheet,
-            nearestBlockSenderAlert: nearestBlockSenderAlert,
-            nearestBlockSendersList: nearestBlockSendersList,
-            nearestReportJunkMessagesActionsPanel: nearestReportJunkMessagesActionsPanel,
-            nearestReportedForPhishingMessagesAlert: nearestReportedForPhishingMessagesAlert,
-            nearestReportedForDisplayProblemMessageAlert: nearestReportedForDisplayProblemMessageAlert,
-            nearestShareMailLinkPanel: nearestShareMailLinkPanel,
-            nearestMessagesToSnooze: nearestMessagesToSnooze,
-            messagesToDownload: messagesToDownload
-        )
     }
 }
