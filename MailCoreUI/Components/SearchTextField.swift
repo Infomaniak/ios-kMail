83df2c9e092cf60e235f489de2b4fcb02670fd54
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-05-08 09:26:43 +0200

    fix: Prevent search field bug because of introspection

--- MailCoreUI/Components/SearchTextField.swift
+++ MailCoreUI/Components/SearchTextField.swift
@@ -22,10 +22,61 @@ import MailCore
 import MailResources
 import SwiftUI
 
+// This is hack because we can't use introspect because of a bug and we also can't use standard focus modifier in a toolbar
+private struct TextFieldFocuserView: UIViewRepresentable {
+    private static let maxSearchDepth = 10
+
+    private class TextFieldFinderUIView: UIView {
+        weak var textField: UITextField?
+        var initialFocusDone = false
+
+        override func didMoveToWindow() {
+            super.didMoveToWindow()
+
+            guard !initialFocusDone else {
+                return
+            }
+
+            guard let textField else {
+                findTextField(view: self)
+                textField?.becomeFirstResponder()
+                initialFocusDone = true
+                return
+            }
+
+            textField.becomeFirstResponder()
+            initialFocusDone = true
+        }
+
+        private func findTextField(view: UIView, level: Int = 0) {
+            if let textField = view as? UITextField {
+                self.textField = textField
+                return
+            }
+
+            guard level < TextFieldFocuserView.maxSearchDepth else {
+                return
+            }
+
+            for subview in view.subviews {
+                findTextField(view: subview, level: level + 1)
+            }
+
+            if let superview = view.superview {
+                findTextField(view: superview, level: level + 1)
+            }
+        }
+    }
+
+    func makeUIView(context: Context) -> UIView {
+        return TextFieldFinderUIView()
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
 public struct SearchTextField: View {
     @EnvironmentObject private var mainViewState: MainViewState
-
-    @State private var initialFocusDone = false
 
     @Binding var value: String
 
@@ -46,6 +97,11 @@ public struct SearchTextField: View {
                     .foregroundStyle(MailResourcesAsset.textTertiaryColor)
             }
             TextField(MailResourcesStrings.Localizable.searchFieldPlaceholder, text: $value)
+                .overlay {
+                    TextFieldFocuserView()
+                        .frame(width: 0, height: 0)
+                        .accessibility(hidden: true)
+                }
                 .autocorrectionDisabled()
                 .submitLabel(.search)
                 .foregroundStyle(value.isEmpty
@@ -53,13 +109,6 @@ public struct SearchTextField: View {
                     : MailResourcesAsset.textPrimaryColor)
                 .onSubmit {
                     onSubmit()
-                }
-                .introspect(.textField, on: .iOS(.v15, .v16, .v17, .v18)) { textField in
-                    guard !initialFocusDone else { return }
-                    DispatchQueue.main.async {
-                        textField.becomeFirstResponder()
-                        initialFocusDone = true
-                    }
                 }
                 .accessibilityAction(.escape) {
                     mainViewState.isShowingSearch = false
