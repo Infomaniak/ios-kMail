a6078e75b4b4947f78672da82f76947f43541287
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-05-21 16:56:35 +0200

    feat: Get correct list of messages without reactions

--- MailCore/Models/Thread.swift
+++ MailCore/Models/Thread.swift
@@ -328,7 +328,7 @@ public extension Thread {
 
     /// Re-generate `Thread` properties given the messages it contains.
     func recomputeOrFail() throws {
-        messages = messages.sorted { $0.internalDate.compare($1.internalDate) == .orderedAscending }.toRealmList()
+        messages = messages.sortedByDate().toRealmList()
         messagesWithoutReactions = List()
 
         guard let lastMessageFromFolder else {
