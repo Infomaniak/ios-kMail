47988069ca789d2f51b9a726305d30ffffbe435b
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-05-21 09:02:50 +0200

    feat: Add `emoji_reactions_per_message` query

--- MailCore/Models/Recipient.swift
+++ MailCore/Models/Recipient.swift
@@ -64,24 +64,6 @@ public final class RecipientsList: EmbeddedObject, Encodable {
     public func contains(where predicate: (Recipient) -> Bool) -> Bool {
         return recipients.contains(where: predicate)
     }
-
-    override public init() {
-        super.init()
-    }
-
-    public convenience init(recipients: [Recipient]) {
-        self.init()
-        self.recipients = recipients.toRealmList()
-    }
-
-    public func encode(to encoder: any Encoder) throws {
-        var container = encoder.unkeyedContainer()
-        try container.encode(contentsOf: recipients)
-    }
-
-    public func append(recipients: [Recipient]) {
-        self.recipients.append(objectsIn: recipients)
-    }
 }
 
 public final class Recipient: EmbeddedObject, Correspondent, Codable {
