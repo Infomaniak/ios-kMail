1d7078e994b2b2109bff7c4c740b1747eefcd34c
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2025-05-22 16:30:20 +0200

    feat: EncryptionAdView almost done

--- Mail/Views/New Message/ComposeMessageView.swift
+++ Mail/Views/New Message/ComposeMessageView.swift
@@ -334,6 +334,9 @@ struct ComposeMessageView: View {
         .sheet(isPresented: $aiModel.isShowingProposition) {
             AIPropositionView(aiModel: aiModel)
         }
+        .sheet(isPresented: $isShowingEncryptPanel) {
+            EncryptionAdView { enableEncryption() }
+        }
         .environmentObject(draftContentManager)
         .matomoView(view: ["ComposeMessage"])
         .scheduleFloatingPanel(
