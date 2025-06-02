4716c96ee9c6fb92bd82a888c37753f16acac4ea
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-06-02 18:01:25 +0200

    feat: Add reaction

--- Mail/Views/Thread/Message/MessageView.swift
+++ Mail/Views/Thread/Message/MessageView.swift
@@ -76,9 +76,7 @@ struct MessageView: View {
                             messageUid: message.uid
                         )
 
-                        if message.hasReactions {
-                            MessageReactionsView(reactions: message.reactions)
-                        }
+                        MessageReactionsView(reactions: message.reactions)
                     }
                 }
             }
