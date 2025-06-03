ff715efe636c57fcba8281beedd52931173137ac
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2025-06-03 12:58:33 +0200

    fix: Use new api return

--- Mail/Views/New Message/ComposeMessageView.swift
+++ Mail/Views/New Message/ComposeMessageView.swift
@@ -445,7 +445,7 @@ struct ComposeMessageView: View {
     }
 
     private func enableEncryption() {
-        mailboxManager.updateRecipientsAutoUncrypt(draft: draft)
+        mailboxManager.updateRecipientsHosted(draft: draft)
 
         if let liveDraft = draft.thaw() {
             try? liveDraft.realm?.write {
