03b27876a754cb886b17a98afa71932ac09d5ccc
Matthieu Déglon <matthieu.deglon@infomaniak.com>
2025-06-17 14:59:48 +0200

    feat: Add servers unavailable status on ThreadListView(Header)

--- Mail/Views/Thread List/ThreadListHeader.swift
+++ Mail/Views/Thread List/ThreadListHeader.swift
@@ -25,6 +25,7 @@ import InfomaniakDI
 import MailCore
 import MailCoreUI
 import MailResources
+import OSLog
 import RealmSwift
 import SwiftUI
 
@@ -88,7 +89,12 @@ struct ThreadListHeader: View {
     @LazyInjectService private var matomo: MatomoUtils
 
     @AppStorage(UserDefaults.shared.key(.accentColor)) private var accentColor = DefaultPreferences.accentColor
+
+    @State private var showMailApiUnavailable: Bool
+
     @StateObject private var folderObserver: ThreadListHeaderFolderObserver
+    @StateObject private var viewModel: ThreadListViewModel
+
     @ObservedObject private var networkMonitor = NetworkMonitor.shared
 
     let isMultipleSelectionEnabled: Bool
@@ -99,11 +105,14 @@ struct ThreadListHeader: View {
     init(isMultipleSelectionEnabled: Bool,
          folder: Folder,
          unreadFilterOn: Binding<Bool>,
-         isRefreshing: Bool) {
+         isRefreshing: Bool,
+         viewModel: ThreadListViewModel) {
         self.isMultipleSelectionEnabled = isMultipleSelectionEnabled
         _unreadFilterOn = unreadFilterOn
+        _showMailApiUnavailable = State(initialValue: false)
         self.isRefreshing = isRefreshing
         _folderObserver = StateObject(wrappedValue: ThreadListHeaderFolderObserver(folder: folder))
+        _viewModel = StateObject(wrappedValue: viewModel)
     }
 
     var body: some View {
@@ -112,6 +121,7 @@ struct ThreadListHeader: View {
                 if !networkMonitor.isConnected {
                     NoNetworkView()
                 }
+
                 if isRefreshing {
                     HStack(spacing: IKPadding.mini) {
                         ProgressView()
@@ -120,11 +130,18 @@ struct ThreadListHeader: View {
                             .textStyle(.bodySmallSecondary)
                     }
                 } else {
-                    if let lastUpdateText = folderObserver.lastUpdateText {
-                        Text(MailResourcesStrings.Localizable.threadListHeaderLastUpdate(lastUpdateText))
-                            .textStyle(.bodySmallSecondary)
+                    if networkMonitor.isConnected && showMailApiUnavailable {
+                        NoMailApiResponseView()
+                    } else {
+                        if let lastUpdateText = folderObserver.lastUpdateText {
+                            Text(MailResourcesStrings.Localizable.threadListHeaderLastUpdate(lastUpdateText))
+                                .textStyle(.bodySmallSecondary)
+                        }
                     }
                 }
+            }
+            .onChange(of: isRefreshing) { _ in
+                checkMailApiAvailability()
             }
             .frame(maxWidth: .infinity, alignment: .leading)
 
@@ -146,6 +163,18 @@ struct ThreadListHeader: View {
         .padding(.bottom, value: .small)
         .padding([.leading, .trailing], value: .medium)
         .background(accentColor.navBarBackground.swiftUIColor)
+    }
+
+    private func checkMailApiAvailability() {
+        Task {
+            do {
+                _ = try await viewModel.mailboxManager.apiFetcher.checkAPIStatus()
+                showMailApiUnavailable = false
+            } catch {
+                Logger.general.error("Error while pinging the Mail API: \(error)")
+                showMailApiUnavailable = true
+            }
+        }
     }
 }
 
@@ -180,12 +209,22 @@ extension ToggleStyle where Self == UnreadToggleStyle {
     ThreadListHeader(isMultipleSelectionEnabled: false,
                      folder: PreviewHelper.sampleFolder,
                      unreadFilterOn: .constant(false),
-                     isRefreshing: false)
+                     isRefreshing: false,
+                     viewModel: ThreadListViewModel(
+                         mailboxManager: PreviewHelper.sampleMailboxManager,
+                         frozenFolder: PreviewHelper.sampleFolder,
+                         selectedThreadOwner: PreviewHelper.mockSelectedThreadOwner
+                     ))
 }
 
 #Preview {
     ThreadListHeader(isMultipleSelectionEnabled: false,
                      folder: PreviewHelper.sampleFolder,
                      unreadFilterOn: .constant(true),
-                     isRefreshing: false)
+                     isRefreshing: false,
+                     viewModel: ThreadListViewModel(
+                         mailboxManager: PreviewHelper.sampleMailboxManager,
+                         frozenFolder: PreviewHelper.sampleFolder,
+                         selectedThreadOwner: PreviewHelper.mockSelectedThreadOwner
+                     ))
 }
