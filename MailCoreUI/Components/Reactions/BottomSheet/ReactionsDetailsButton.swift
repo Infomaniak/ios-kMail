1cbf74d0e198e53f29ced6de137394c7596bd9e3
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-07-17 09:40:27 +0200

    feat: Split views into several files

---
+++ MailCoreUI/Components/Reactions/BottomSheet/ReactionsDetailsButton.swift
@@ -0,0 +1,72 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailResources
+import SwiftUI
+
+extension ReactionsDetailsView.SelectionType {
+    var buttonLabel: String {
+        switch self {
+        case .all:
+            return MailResourcesStrings.Localizable.buttonAllReactions
+        case .reaction(let uIMessageReaction):
+            return uIMessageReaction.formatted()
+        }
+    }
+}
+
+struct ReactionsDetailsButton: View {
+    @Binding var currentSelection: ReactionsDetailsView.SelectionType
+
+    let selectionType: ReactionsDetailsView.SelectionType
+    let namespace: Namespace.ID
+
+    private var isSelected: Bool {
+        currentSelection == selectionType
+    }
+
+    private let animation = Animation.default
+
+    var body: some View {
+        Button {
+            withAnimation(animation) {
+                currentSelection = selectionType
+            }
+        } label: {
+            Text(selectionType.buttonLabel)
+                .textStyle(.bodyMedium)
+                .padding(.horizontal, value: .small)
+                .padding(.vertical, value: .mini)
+        }
+        .overlay(alignment: .bottom) {
+            if currentSelection == selectionType {
+                Capsule()
+                    .fill(Color.accentColor)
+                    .frame(height: 2)
+                    .matchedGeometryEffect(id: "SelectedCapsule", in: namespace)
+            }
+        }
+        .animation(animation, value: currentSelection)
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    @Previewable @Namespace var animation
+    ReactionsDetailsButton(currentSelection: .constant(.all), selectionType: .all, namespace: animation)
+}
---
+++ MailCoreUI/Components/Reactions/BottomSheet/ReactionsDetailsButtonsView.swift
@@ -0,0 +1,54 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import MailResources
+import SwiftUI
+
+struct ReactionsDetailsButtonsView: View {
+    @Namespace private var animation
+
+    @Binding var currentSelection: ReactionsDetailsView.SelectionType
+
+    let reactions: [UIMessageReaction]
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Rectangle()
+                .fill(MailResourcesAsset.elementsColor.swiftUIColor)
+                .frame(height: 1)
+
+            ScrollView(.horizontal) {
+                HStack {
+                    ReactionsDetailsButton(currentSelection: $currentSelection, selectionType: .all, namespace: animation)
+                    ForEach(reactions) { reaction in
+                        ReactionsDetailsButton(
+                            currentSelection: $currentSelection,
+                            selectionType: .reaction(reaction),
+                            namespace: animation
+                        )
+                    }
+                }
+                .padding(.horizontal, value: .micro)
+            }
+        }
+    }
+}
+
+#Preview {
+    ReactionsDetailsButtonsView(currentSelection: .constant(.all), reactions: PreviewHelper.uiReactions)
+}
