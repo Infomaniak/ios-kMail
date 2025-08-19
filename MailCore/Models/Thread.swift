efb06e7461708d12961834ea94f5c7b65f6c0128
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-08-19 07:55:24 +0200

    fix: Filter messages to display

--- MailCore/Models/Thread.swift
+++ MailCore/Models/Thread.swift
@@ -298,7 +298,7 @@ public class Thread: Object, Decodable, Identifiable {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         uid = try container.decode(String.self, forKey: .uid)
         messages = try container.decode(List<Message>.self, forKey: .messages)
-        messagesToDisplay = self.messages
+        messagesToDisplay = messages.filter { !$0.isReaction }.toRealmList()
         unseenMessages = try container.decode(Int.self, forKey: .unseenMessages)
         from = try container.decode(List<Recipient>.self, forKey: .from)
         to = try container.decode(List<Recipient>.self, forKey: .to)
