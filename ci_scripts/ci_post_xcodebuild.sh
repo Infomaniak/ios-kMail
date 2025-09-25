4395fe5d8e0230c7f11ba758012ead4fd1828e0e
Valentin <valentin.perignon@infomaniak.com>
2025-09-25 08:16:35 +0200

    chore: Do not push symbols to sentry

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -11,23 +11,6 @@ export PATH="$HOME/.local/bin:$PATH"
 mise install
 eval "$(mise activate bash --shims)"
 
-
-if [[ -n $CI_ARCHIVE_PATH ]]; then
-    retries=0
-    max_retries=3
-    until [ $retries -ge $max_retries ]
-    do
-        sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN upload-dif --org sentry --project $SENTRY_PROJECT --include-sources $CI_DERIVED_DATA_PATH && break
-        retries=$((retries+1))
-        echo "Retry $retries/$max_retries for sentry-cli failed."
-        sleep 2
-    done
-
-    if [ $retries -eq $max_retries ]; then
-        echo "sentry-cli failed after $max_retries attempts."
-    fi
-else
-    echo "Archive path isn't available. Unable to run dSYMs uploading script."
-fi
+# do not push to sentry for tets
 
 ci_scripts/push_tag.sh
