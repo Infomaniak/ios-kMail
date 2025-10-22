da579583b4d76ad2846ee0da95a73f79444e8440
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-10-22 15:54:49 +0200

    chore: Update packages

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -73,7 +73,7 @@ let package = Package(
         .package(url: "https://github.com/Infomaniak/ios-create-account", .upToNextMajor(from: "23.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.5")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.1.1")),
+        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.3.0")),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.3.0")),
         .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "15.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.1.0")),
