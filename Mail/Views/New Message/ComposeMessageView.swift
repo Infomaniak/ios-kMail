12bcd71e04dc033ce8d21b7107c99522a542b1fa
Elena Willen <elena.willen@infomaniak.com>
2026-06-10 14:55:12 +0200

    fix: Rename isShowingSendOptionsPanel

--- Mail/Views/New Message/ComposeMessageView.swift
+++ Mail/Views/New Message/ComposeMessageView.swift
@@ -103,7 +103,7 @@ struct ComposeMessageView: View {
     @State private var autocompletionType: ComposeViewFieldType?
     @State private var currentSignature: Signature?
     @State private var initialAttachments = [Attachable]()
-    @State private var isShowingSchedulePanel = false
+    @State private var isShowingSendOptionsPanel = false
     @State private var isShowingMyKSuitePanel = false
     @State private var isShowingKSuiteProPanel = false
     @State private var isShowingMailPremiumPanel = false
@@ -289,7 +289,7 @@ struct ComposeMessageView: View {
                         isShowingMyKSuitePanel: $isShowingMyKSuitePanel,
                         isShowingMailPremiumPanel: $isShowingMailPremiumPanel,
                         isShowingEncryptStatePanel: $isShowingEncryptStatePanel,
-                        isShowingSchedulePanel: $isShowingSchedulePanel,
+                        isShowingSendOptionsPanel: $isShowingSendOptionsPanel,
                         draft: draft,
                         isEditorFocused: focusedField == .editor,
                         selectedText: selectedText
@@ -446,7 +446,7 @@ struct ComposeMessageView: View {
         .environmentObject(draftContentManager)
         .matomoView(view: ["ComposeMessage"])
         .sendOptionFloatingPanel(
-            isPresented: $isShowingSchedulePanel,
+            isPresented: $isShowingSendOptionsPanel,
             isUpdating: false,
             initialDate: draft.scheduleDate,
             completionHandler: didScheduleDraft
