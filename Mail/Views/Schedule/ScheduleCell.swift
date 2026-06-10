304a4b448792c8e7254c6a96bc3ae1388dd19bf8
Elena Willen <elena.willen@infomaniak.com>
2026-06-10 13:42:19 +0200

    feat: Add ScheduleCell

---
+++ Mail/Views/Schedule/ScheduleCell.swift
@@ -0,0 +1,73 @@
+/*
+ Infomaniak Mail - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import DesignSystem
+import MailCoreUI
+import MailResources
+import SwiftUI
+
+struct ScheduleCell: View {
+    let option: ScheduleOption
+    let isSelected: Bool
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                VStack(alignment: .leading, spacing: IKPadding.micro) {
+                    Text(option.title)
+                        .textStyle(.body)
+
+                    if option.isCustom {
+                        if isSelected, let date = option.date {
+                            Text(date, format: .scheduleOption)
+                                .textStyle(.bodySmallSecondary)
+                        }
+                    } else if let date = option.date {
+                        Text(date, format: .scheduleOption)
+                            .textStyle(.bodySmallSecondary)
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+                if isSelected {
+                    MailResourcesAsset.check.iconSize(.medium)
+                        .foregroundStyle(Color.accentColor)
+                }
+
+                if option.isCustom, !isSelected {
+                    ChevronIcon(direction: .right, shapeStyle: MailResourcesAsset.textSecondaryColor.swiftUIColor)
+                }
+            }
+            .padding(.leading, IKIconSize.large.rawValue + IKPadding.mini)
+            .padding(.trailing, value: .medium)
+        }
+        .padding(.vertical, value: .medium)
+        .padding(.leading, value: .medium)
+    }
+}
+
+#Preview {
+    VStack(spacing: 0) {
+        ScheduleCell(option: .thisAfternoon, isSelected: false) {}
+        IKDivider(type: .item)
+        ScheduleCell(option: .thisEvening, isSelected: true) {}
+        IKDivider(type: .item)
+        ScheduleCell(option: .tomorrowMorning, isSelected: false) {}
+    }
+}
