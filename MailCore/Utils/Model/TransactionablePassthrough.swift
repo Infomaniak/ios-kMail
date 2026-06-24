890048e23f98cb388dd795472ac370e891417851
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-24 12:45:15 +0200

    fix: Add method and update tag for breaking core version

--- MailCore/Utils/Model/TransactionablePassthrough.swift
+++ MailCore/Utils/Model/TransactionablePassthrough.swift
@@ -43,6 +43,10 @@ extension TransactionablePassthrough {
         return transactionExecutor.fetchResults(ofType: type, filtering: filtering)
     }
 
+    public func writeTransaction(withExpiringActivity enabled: Bool, withRealm realmClosure: (Realm) throws -> Void) throws {
+        try transactionExecutor.writeTransaction(withExpiringActivity: enabled, withRealm: realmClosure)
+    }
+
     public func writeTransaction(withRealm realmClosure: (Realm) throws -> Void) throws {
         try transactionExecutor.writeTransaction(withRealm: realmClosure)
     }
