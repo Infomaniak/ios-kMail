0060cdc663366f569964c46f3a6c71be26216e0d
Elena Willen <elena.willen@infomaniak.com>
2026-06-30 14:30:48 +0200

    fix: Change Binding

--- Mail/Views/New Message/ComposeMessageView.swift
+++ Mail/Views/New Message/ComposeMessageView.swift
@@ -289,11 +289,11 @@ struct ComposeMessageView: View {
                         isShowingMailPremiumPanel: $isShowingMailPremiumPanel,
                         isShowingEncryptStatePanel: $isShowingEncryptStatePanel,
                         isShowingSendOptionsPanel: $isShowingSendOptionsPanel,
-                        selectedScheduleOption: $selectedScheduleOption,
-                        selectedReminderOption: $selectedReminderOption,
                         draft: draft,
                         isEditorFocused: focusedField == .editor,
-                        selectedText: selectedText
+                        selectedText: selectedText,
+                        selectedScheduleOption: selectedScheduleOption,
+                        selectedReminderOption: selectedReminderOption
                     )
                     .environmentObject(attachmentsManager)
                 }
