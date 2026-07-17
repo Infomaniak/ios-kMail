3060bfbb942b54462b60f781c10c6ae9288db221
Killian Mathias <killian.mathias@infomaniak.com>
2026-07-17 15:23:46 +0200

    fix: Update reminder chip visibility logic

--- MailCore/Models/Thread.swift
+++ MailCore/Models/Thread.swift
@@ -159,7 +159,7 @@ public class Thread: Object, Decodable, Identifiable {
     }
 
     public var hasReminder: Bool {
-        return messages.last?.hasReminder == true
+        return messages.contains { $0.showReminderChip == true }
     }
 
     public func updateUnseenMessages() {
