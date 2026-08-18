dac6812c1857d3c34c1ce7dc90304bec37f272da
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-08-18 10:40:42 +0200

    feat: Link with new branch version of Swift Rich HTML Editor and add logic to display new command

--- Mail/Views/New Message/ComposeMessageBodyView.swift
+++ Mail/Views/New Message/ComposeMessageBodyView.swift
@@ -73,6 +73,19 @@ struct ComposeMessageBodyView: View {
         return MessageWebViewUtils.loadCSS(for: .editor(aliases: aliases)).joined()
     }
 
+    private var commands: [HTMLEditorCustomAction] {
+        [
+            HTMLEditorCustomAction(title: "Coller et adapter le style") { editor in
+                let pasteboard = UIPasteboard.general
+                guard pasteboard.numberOfItems > 0 else { return }
+
+                if let text = pasteboard.string, !text.isEmpty {
+                    editor.pasteAndMatchStyle(text)
+                }
+            }
+        ]
+    }
+
     var body: some View {
         VStack {
             #if os(macOS) || targetEnvironment(macCatalyst)
@@ -89,9 +102,12 @@ struct ComposeMessageBodyView: View {
             }
             #endif
             AttachmentsHeaderView()
-            RichHTMLEditor(html: $draftBody, selection: $selectedText, textAttributes: textAttributes,
+            RichHTMLEditor(html: $draftBody,
+                           selection: $selectedText,
+                           textAttributes: textAttributes,
                            spellCheckEnabled: !isEnvironmentCatalyst,
-                           autoCorrectEnabled: !isEnvironmentCatalyst)
+                           autoCorrectEnabled: !isEnvironmentCatalyst,
+                           commands: commands)
                 .focused($focusedField, equals: .editor)
                 .onEditorLoaded(perform: editorDidLoad)
                 .editorCSS(customCSS)
