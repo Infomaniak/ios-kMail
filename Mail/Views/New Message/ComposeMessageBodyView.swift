a5b61512593c6df56a3096a480e70b0e6e94d289
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-08-18 13:14:39 +0200

    fix: Apply review

--- Mail/Views/New Message/ComposeMessageBodyView.swift
+++ Mail/Views/New Message/ComposeMessageBodyView.swift
@@ -73,18 +73,16 @@ struct ComposeMessageBodyView: View {
         return MessageWebViewUtils.loadCSS(for: .editor(aliases: aliases)).joined()
     }
 
-    private var commands: [HTMLEditorCustomAction] {
-        [
-            HTMLEditorCustomAction(title: MailResourcesStrings.Localizable.pasteAdjustStyleAction) { editor in
-                let pasteboard = UIPasteboard.general
-                guard pasteboard.numberOfItems > 0 else { return }
+    @State private var commands: [HTMLEditorCustomAction] = [
+        HTMLEditorCustomAction(title: MailResourcesStrings.Localizable.pasteAdjustStyleAction) { editor in
+            let pasteboard = UIPasteboard.general
+            guard pasteboard.numberOfItems > 0 else { return }
 
-                if let text = pasteboard.string, !text.isEmpty {
-                    editor.pasteAndMatchStyle(text)
-                }
+            if let text = pasteboard.string, !text.isEmpty {
+                editor.pasteAndMatchStyle(text)
             }
-        ]
-    }
+        }
+    ]
 
     var body: some View {
         VStack {
